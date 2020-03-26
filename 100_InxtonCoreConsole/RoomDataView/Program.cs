167b5f231f4e8387aba4bed036cd78aa891e8b56
PTKu <peter.kurhajec@inxton.com>
2020-03-26 13:51:13 +0100

    100 tutorial project added

---
+++ 100_InxtonCoreConsole/RoomDataView/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using RoomController;
+
+namespace RoomDataView
+{
+    class Program
+    {        
+        private static void OutputDataToConsole(object o)
+        {
+            var RoomData = RoomController.Entry.RoomControllerPlc.MAIN.RoomData;
+
+            Console.WriteLine("Time: " + DateTime.Now.ToString() + " | " +
+                RoomData.Temperature.AttributeName + ": " + RoomData.Temperature.Cyclic.ToString() + " | " +
+                RoomData.Humidity.AttributeName + ": " + RoomData.Humidity.Cyclic.ToString() + " | " +
+                RoomData.VOC.AttributeName + ": " + RoomData.VOC.Cyclic.ToString() + " | " +
+                RoomData.CO2.AttributeName + ": " + RoomData.CO2.Cyclic.ToString() + " | ");
+        }
+
+        static void Main(string[] args)
+        {            
+            Entry.RoomControllerPlc.Connector.ReadWriteCycleDelay = 100;
+            Entry.RoomControllerPlc.Connector.BuildAndStart();
+            Timer t = new Timer(OutputDataToConsole, null, 0, 1000);
+
+            Console.ReadLine();            
+        }
+    }
+}
